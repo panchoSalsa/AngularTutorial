49396e6a282f5e28bc594ded515238feef69887f
Franco <frfranco@uci.edu>
2017-03-13 09:10:11 -0700

    Refactoring the classifieds route

--- scripts/app.js
+++ scripts/app.js
@@ -8,19 +8,23 @@ angular
 		$stateProvider
 			.state('one', {
 				url: '/stateone',
-				template: '<h1>StateOne</h1>'
+				template: '<h1>{{stateone.message}}</h1>',
+				controller: 'stateOneCtrl as stateone'
 			})
 			.state('two', {
 				url: '/statetwo',
-				template: '<h1>StateTwo</h1> <md-button ui-sref="two.more">Deeper</md-button><ui-view></ui-view>'
+				template: '<h1>StateTwo</h1> <md-button ui-sref="two.more">Deeper</md-button><ui-view></ui-view>',
+				controller: 'stateTwoCtrl as statetwo'
 			})	
 			.state('two.more', {
 				url: '/more',
 				template: '<p>Nested State</p>'
 			});		
 	})
-	.directive("helloWorld", function() {
-		return {
-			template: "<h1>{{message}}</h1>"
-		};
-	});
+
+	.controller("stateOneCtrl", function() {
+		var vm = this;
+		vm.message = "hello from state one!";
+	}
+
+	);
